6a7677bfe74f1d752b145648a184606cb3e3f6b4
Timothy Liu <timothytliu@gmail.com>
2011-03-09 00:15:25 -0800

    first commit

---
+++ src/.svn/text-base/myclickatell.py.svn-base
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+"""
+myclickatell.py
+
+Created by Niels Joubert on 2009-02-14.
+Copyright (c) 2009 __MyCompanyName__. All rights reserved.
+
+USAGE: import Clickatell from myclickatell
+
+"""
+
+import urllib
+import urllib2
+import re
+import math
+
+class Clickatell:
+
+    def __init__(self, username, password, app_id):
+        self.username = username
+        self.password = password
+        self.app_id = app_id
+        self.send_uri = "http://api.clickatell.com/http/sendmsg"
+        self.singleMsgLen = 160
+    
+    def sendMsg(self, cellnr, msg):
+        """
+            Returns a list of (True/False, ID, [error message]) tuples.
+            The length of this list tells you how many messages you sent.
+        """
+    
+        metaData = {'api_id' : self.app_id,
+            'user' : self.username,
+            'password' :  self.password,
+            'to' : cellnr,
+        }
+        
+        msgLen = len(msg)
+        numMsgs = int(math.ceil(float(msgLen)/float(self.singleMsgLen)))
+            
+        result = []
+        for i in range(numMsgs):
+            text = msg[(i*self.singleMsgLen) : (i+1)*self.singleMsgLen]
+            result.append(self.__sendSingleMsg(metaData, text))
+        
+        return result
+        
+    def getMaxMessageLen():
+        return self.singleMsgLen
+        
+    def __sendSingleMsg(self, metaData, text):
+        """        
+        Returns:
+            on success:
+                (True, 'ID')
+            on fail:
+                (False, 'error-code', 'reason')
+        """
+        values = dict(metaData)
+        values['text'] = text
+        data = urllib.urlencode(values)
+        req = urllib2.Request(self.send_uri, data)
+        response  = urllib2.urlopen(req)
+        the_page = response.read()
+        
+        m = re.search('(ID: (.*))|(ERR: (\d*), (.*))', the_page)
+        if m == None:
+            return (False, '0', 'REGEX DID NOT MATCH')
+        else:
+            if m.group(1) != None:
+                return (True, m.group(2))
+            else:
+                return (False, m.group(4), m.group(5))
+        return None
+        
+
+
+"""
+    EXAMPLE USAGE:
+    
+    c = Clickatell("njoubert", "rsd887", "3076162")
+    cellnr = '16508231662'
+    msg = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam fermentum orci sit amet mi. Donec eget massa. Aenean aliquet, magna nullam. A A A A A A A A A A A'
+    print len(msg)
+    print c.sendMsg(cellnr, msg)
+    
+"""
---
+++ src/smsupc.py
@@ -0,0 +1,100 @@
+import sys
+import re
+import productSearch
+import myclickatell
+
+
+def massagePrice(price):
+    roundingPoint = "%.2f"
+    if (price > 20):
+        return int(round(float(price)))
+    
+def massageName(name, limit):
+    newName = str(name)[0:limit]
+    if len(name) > limit:
+        newName += "..."
+    return newName
+
+# TODO: argument checking
+phoneNumber = sys.argv[1]
+sms = sys.argv[2]
+
+# Test cases
+#phoneNumber = "fake"
+#sms = "2               410000039144 013803050844"
+#sms = "san francisco 2              410000039144 013803050844"
+
+smsList = [x for x in sms.split(" ") if len(x) > 0]
+locationList = []
+for i in range(len(smsList)):
+    if re.match("[a-zA-Z]+", smsList[i]):
+        # handle city name
+        locationList.append(smsList[i])
+    else:
+        break
+    
+if len(locationList) == 0:
+    # TODO: handle zip code
+    print "HANDLE ZIP CODE!!!!"
+    pass
+
+smsList = smsList[len(locationList):]
+location = " ".join(locationList)
+print location
+print smsList
+
+c = myclickatell.Clickatell("njoubert", "rsd887", "3076162")
+# not actually number of results returned, but number considered
+numGoogleResults = 30
+failList = []
+for upc in smsList:
+    
+    validLocal = False
+    validOnline = False
+    response = ""
+    avgPrice = 0
+    
+    # local
+    if len(location) > 0:
+        baseName = productSearch.getProductName(upc)
+        if baseName != None:
+            localData = productSearch.localDataParser().getLocalProductData(baseName, location)
+            avgPrice = localData[0]
+            if len(localData) > 0:
+                validLocal = True
+                response += "Local: " + massageName(localData[1], 25) + " $" + str(massagePrice(localData[3])) + " at " + massageName(localData[2], 15) + "\n"
+            else:
+                response += "Local: No Results\n"
+        else:
+            response += "Local: No Results\n"
+    
+    # online    
+    googleData = productSearch.getGoogleProductData(upc, numGoogleResults)
+    if googleData != None:
+        validOnline = True
+        item = googleData[0]
+        avgPrice += googleData[1]
+        if validLocal:
+            avgPrice /= 2
+        response += "Web: " + massageName(item[0], 25) + " $" + str(massagePrice(item[2])) + " at " + massageName(item[1], 15) + "\n"
+    else:
+        response += "Web: No Results\n"
+    
+    # send sms
+    if validLocal or validOnline:
+        response += "Avg: $" + str(massagePrice(avgPrice)) + "\n"
+        response += upc
+        print "---------------------------"
+        print response
+        c.sendMsg(phoneNumber, response)
+    else:
+        failList.append(upc)
+
+if len(failList) > 0:
+    response = ""
+    for upc in failList:
+        response += upc + ": No Results\n"
+    print "---------------------"
+    print response
+    c.sendMsg(phoneNumber, response)
+    
